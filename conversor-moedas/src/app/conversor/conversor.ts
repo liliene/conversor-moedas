02338a9b0f168e71321a5672c25f75f333894d3b
liliene <108309670+liliene@users.noreply.github.com>
2025-10-03 13:16:10 -0300

    Create conversor.ts

---
+++ conversor-moedas/src/app/conversor/conversor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conversor',
+  imports: [],
+  templateUrl: './conversor.html',
+  styleUrl: './conversor.css'
+})
+export class Conversor {
+
+}
