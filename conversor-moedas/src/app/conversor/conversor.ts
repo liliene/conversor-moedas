28052a961e614bca5b3eedf9e89288f15ec8d526
liliene <108309670+liliene@users.noreply.github.com>
2025-10-03 13:37:26 -0300

    Update conversor.ts

--- conversor-moedas/src/app/conversor/conversor.ts
+++ conversor-moedas/src/app/conversor/conversor.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-conversor',
-  imports: [],
-  templateUrl: './conversor.html',
-  styleUrl: './conversor.css'
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './conversor.component.html',
+  styleUrl: './conversor.component.css'
 })
-export class Conversor {
+export class ConversorComponent {
+  // Variáveis para armazenar os dados do formulário
+  valor: number = 1;
+  moedaDe: string = 'USD';
+  moedaPara: string = 'BRL';
 
+  // Variável para armazenar o resultado da conversão
+  resultado: number = 0;
 }
